3c0abb1fa8ab941554f7f82db894eb00075c71cf
Evan <Donoyster@icloud.com>
2025-03-26 15:32:21 -0700

    wdwd
    wdw

--- PlayGameBasic2UnitP8Perez/Assets/Scripts/SpawnManager.cs
+++ PlayGameBasic2UnitP8Perez/Assets/Scripts/SpawnManager.cs
@@ -5,24 +5,26 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] animalPrefabs;
-    private float spawnRangeX = 20;
-    private float spawnPosZ = 25
-       ;
+    private float spawnRangeX = 10;
+    private float spawnPosZ = 30;
+    private float spawnInterval = 1.5f;
+    private float startDelay = 2;
+       
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX,spawnRangeX),0, spawnPosZ);
-            int animalIndex = Random.Range(0, animalPrefabs.Length);
-            Instantiate(animalPrefabs[animalIndex], spawnPos,
-                animalPrefabs[animalIndex].transform.rotation);
-        }
+        
+    }
+    void SpawnRandomAnimal()
+    {
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Instantiate(animalPrefabs[animalIndex], spawnPos,animalPrefabs[animalIndex].transform.rotation);
     }
 }
